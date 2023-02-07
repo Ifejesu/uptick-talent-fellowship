c62b306f1d5cd15bd98c1227ffb44ff7832cfc0b
Goodness Ifejesu Olajide <ifejesugoodness97@gmail.com>
2023-02-07 13:39:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import heroImage from "./banner.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="hero">
+        <div className="hero-text">
+          <h1>Take your software engineering career to the next level.</h1>
+          <p>
+            Join the Uptick Talent Engineering Fellowship to unlock your
+            engineering potential with tailored and individualized training,
+            support, and placement into entry-level roles in tech companies.
+          </p>
+          <a
+            href="https://bit.ly/Uptick-Talent-Fellowship"
+            className="apply-button"
+          >
+            APPLY NOW
+          </a>
+        </div>
+        <div className="hero-image">
+          <img src={heroImage} alt="hero" />
+        </div>
+      </div>
     </div>
   );
 }
